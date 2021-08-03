489ef64eb8ac5c0849b67124655c011edb6acfd6
Change <improvetheworld@yandex.ru>
2021-08-03 14:35:27 +0300

    Teams

---
+++ Assets/Game/Editor/Tests/TeamTest.cs
@@ -0,0 +1,37 @@
+﻿using Game.Characters;
+using Game.Characters.Teams;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class TeamTest
+    {
+        [Test]
+        public void Test_Naming()
+        {
+            var team = new Team(1);
+            team.SetName("Yes");
+            team.SetName("No");
+
+            var team2 = new Team(2, "Sure");
+
+            Assert.AreEqual(team.Name, "No");
+            Assert.AreEqual(team2.Name, "Sure");
+        }
+
+        [Test]
+        public void Test_Characters()
+        {
+            var character1 = new Mage(new Stats());
+            var character2 = new Mage(new Stats());
+            var character3 = new Mage(new Stats());
+
+            var team = new Team(1);
+
+            team.AddMembers(character1, character2);
+            team.RemoveMembers(character3);
+
+            Assert.AreEqual(team.Members.Count, 2);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Characters/Teams/Team.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Characters.Teams
+{
+    public class Team
+    {
+        public readonly int ID;
+        public string Name { get; private set; }
+        public readonly IReadOnlyCollection<Team> Allies;
+        public readonly IReadOnlyCollection<Team> Enemies;
+        public readonly IReadOnlyCollection<Character> Members;
+
+        readonly List<Team> _allies;
+        readonly List<Team> _enemies;
+        readonly List<Character> _members;
+
+        public Team(int id)
+        {
+            ID = id;
+
+            _allies = new List<Team>();
+            _enemies = new List<Team>();
+            _members = new List<Character>();
+
+            Allies = _allies.AsReadOnly();
+            Enemies = _enemies.AsReadOnly();
+            Members = _members.AsReadOnly();
+        }
+
+        public Team(int id, string name) : this(id)
+        {
+            SetName(name);
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void AddMembers(params Character[] characters)
+        {
+            _members.AddRange(characters);
+        }
+
+        public void AddAllies(params Team[] allies)
+        {
+            _allies.AddRange(allies);
+        }
+
+        public void AddEnemies(params Team[] enemies)
+        {
+            _enemies.AddRange(enemies);
+        }
+
+        public void RemoveMembers(params Character[] charactersToRemove)
+        {
+            foreach (var member in charactersToRemove)
+            {
+                _members.Remove(member);
+            }
+        }
+
+        public void RemoveAllies(params Team[] alliesToRemove)
+        {
+            foreach (var ally in alliesToRemove)
+            {
+                _allies.Remove(ally);
+            }
+        }
+
+        public void RemoveEnemies(params Team[] enemiesToRemove)
+        {
+            foreach (var enemy in enemiesToRemove)
+            {
+                _enemies.Remove(enemy);
+            }
+        }
+    }
+}
